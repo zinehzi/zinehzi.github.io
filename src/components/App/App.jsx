72520316437924a647116b2a386e9c28a075afa8
Zineh Ziaraty <ziaraty.zi@gmail.com>
2022-09-06 18:52:20 +0430

    Fixing The Fonts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
+            zine ziaraty hastam front-end developer
+
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import 'bootstrap/dist/css/bootstrap.css';
+import './style/custom.scss';
 import './index.scss';
-import 'bootstrap/dist/css/bootstrap.css';
-// import './custom.scss';
-import App from './components/App';
+import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
